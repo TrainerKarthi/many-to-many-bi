f99c961f4d76f85ef77660b69bb34d42ff15e733
TrainerKarthi <vkarthik2502@gmail.com>
2022-05-25 16:35:30 +0530

    many to many cascade has been uploaded

--- src/main/java/com/ty/manytomanybi/Student.java
+++ src/main/java/com/ty/manytomanybi/Student.java
@@ -2,6 +2,7 @@ package com.ty.manytomanybi;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,7 +18,7 @@ public class Student {
 	private String name;
 	private int age;
 
-	@ManyToMany(mappedBy = "student")
+	@ManyToMany(mappedBy = "student",cascade = CascadeType.ALL)
 	private List<Course> courses;
 
 	public int getId() {
